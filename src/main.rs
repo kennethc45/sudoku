07196694627142fdc3909c9882a36476aaca43e7
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2024-02-26 13:54:55 -0600

    Created a board through mutli dimensional vectors and a function that prints the board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+fn print_board() {
+    let board: Vec<Vec<u32>> = vec![
+    // 0's represent empty spaces
+
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0],
+    vec![0,0,0,0,0,0,0,0,0]
+];
+
+    for i in 0..board.len() {
+        // Separates boxes horizontally
+        if (i != 0) && (i % 3 == 0) {
+            println!("- - - - - - - - - - - - -");
+        }
+
+        for j in 0..board[0].len() {
+            // Separates boxes vertically
+            if (j != 0) && (j % 3 == 0) {
+                print!(" | ");
+            }
+
+            if j == 8 {
+                // Prints values for last column 
+                println!("{}", board[i][j]);
+            } else {
+                // Prints all other values w/ a space
+                // Lines values up with the horizontal dash lines 
+                print!("{} ", board[i][j].to_string());
+            }
+        }
+    }
 }
+
+fn main() {
+    print_board();
+}
+
+
+
